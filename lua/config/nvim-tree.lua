b6c828a939a7ab3957df110b73d2f1777a1dac1b
qiaoshouxing <qiaoshouxingsdd@163.com>
2023-10-27 13:24:59 +0800

    Add on_attach key 's' to nvim-tree

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,7 +1,22 @@
 local keymap = vim.keymap
 local nvim_tree = require("nvim-tree")
 
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', 's',   api.node.open.vertical,              opts('Open: Vertical Split'))
+end
+
 nvim_tree.setup {
+  on_attach = my_on_attach,
   auto_reload_on_write = true,
   disable_netrw = false,
   hijack_cursor = false,
