a1d4f77da54ac36eb614b9aa14fb21ece457070c
qiaoshouxing <qiaoshouxingsdd@163.com>
2025-09-29 17:29:05 +0800

    Unuse cursor forcus

--- lua/diagnostic-conf.lua
+++ lua/diagnostic-conf.lua
@@ -44,23 +44,23 @@ vim.keymap.set("n", "<space>qb", function()
 end, { desc = "put buffer diagnostics to qf" })
 
 -- automatically show diagnostic in float win for current line
-api.nvim_create_autocmd("CursorHold", {
-  pattern = "*",
-  callback = function()
-    if #vim.diagnostic.get(0) == 0 then
-      return
-    end
-
-    if not vim.b.diagnostics_pos then
-      vim.b.diagnostics_pos = { nil, nil }
-    end
-
-    local cursor_pos = api.nvim_win_get_cursor(0)
-
-    if not vim.deep_equal(cursor_pos, vim.b.diagnostics_pos) then
-      diagnostic.open_float { width = 100 }
-    end
-
-    vim.b.diagnostics_pos = cursor_pos
-  end,
-})
+--api.nvim_create_autocmd("CursorHold", {
+--  pattern = "*",
+--  callback = function()
+--    if #vim.diagnostic.get(0) == 0 then
+--      return
+--    end
+--
+--    if not vim.b.diagnostics_pos then
+--      vim.b.diagnostics_pos = { nil, nil }
+--    end
+--
+--    local cursor_pos = api.nvim_win_get_cursor(0)
+--
+--    if not vim.deep_equal(cursor_pos, vim.b.diagnostics_pos) then
+--      diagnostic.open_float { width = 100 }
+--    end
+--
+--    vim.b.diagnostics_pos = cursor_pos
+--  end,
+--})
